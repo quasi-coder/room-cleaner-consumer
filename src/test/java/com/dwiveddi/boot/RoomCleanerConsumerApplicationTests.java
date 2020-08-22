9d5c0917e697e7e544da3f3080a44d4c13013fb1
Divyashree <divyashreedwivedi@icloud.com>
2020-08-22 22:33:55 +0530

    RabbitMq consumer with Spring Boot

---
+++ src/test/java/com/dwiveddi/boot/RoomCleanerConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dwiveddi.boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoomCleanerConsumerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
